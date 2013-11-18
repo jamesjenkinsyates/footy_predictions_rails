b826aaae5c9283949190899e0d1df10045c484e5
Rhys Stansfield <rhys@onlinesalesgrowth.com>
2013-11-18 19:26:34 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FootyPredictionsRails::Application.config.secret_key_base = '2759c579241b3dc75bc4bff36b727762160b063a431d00ebfd987c866b720f64b1a19a25deba73c53e5e1be4a1ef7913a056658434778cc83b0c00dff5421b70'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FootyPredictionsRails::Application.config.session_store :cookie_store, key: '_footy_predictions_rails_session'
