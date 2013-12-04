ad98363f3f74254f48d4355ef3e9d38fe8a989b1
Rhys Stansfield <rhys@onlinesalesgrowth.com>
2013-12-04 23:39:52 +0000

    Fixed Devise/Facebook login and implemented basic dashboard functionality

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class DashboardController < ApplicationController
 
   def index 
-    @predictions = current_user.predictions.past
+    @predictions = current_user.predictions
+    # @predictions = Prediction.where(current_user).past
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
+  has_many :predictions
+
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     if user
@@ -21,5 +23,6 @@ class User < ActiveRecord::Base
                           )
       end
     end
+
   end
 end
