f81c856d5592fd3509483c0397d4b6a1f95d6885
Rhys Stansfield <rhys@onlinesalesgrowth.com>
2013-12-07 16:26:23 +0000

    Starting implementation of checking if a match is finished

---
+++ spec/factories/match_factory.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :match, class: Match do
+    home_team "Man United"
+    away_team "Newcastle"
+    match_date_time "2013-12-07 12:45:00"
+  end
+end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,6 +1,17 @@
 require 'spec_helper'
 
 describe Match do
+
   it { should have_many :predictions }
+
+  context 'checking match status' do
+
+    it 'should know if a match has finished' do
+      Timecop.freeze(2013, 12, 07, 15, 45, 00)
+      match = FactoryGirl.build(:match)
+      expect(match.match_finished?).to be_true
+    end
+
+  end
 
 end
