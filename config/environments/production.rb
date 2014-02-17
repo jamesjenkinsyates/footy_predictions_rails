dc8c817b969cd552c759612b24737744baf6a75f
jamesjenkinsyates <james@jamesjenkinsyates.com>
2014-02-17 10:38:30 +0000

    Switched to fontawesome

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ FootyPredictionsRails::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf' )
 
   # Generate digests for assets URLs.
   config.assets.digest = true
